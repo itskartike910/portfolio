d75a522a8ac8b7c151e8d71a7f844492e65c3a06
Kartik kumar <kumarkartik147359@gmail.com>
2024-06-11 00:21:50 +0530

    updated main

--- lib/constants/consts.dart
+++ lib/constants/consts.dart
@@ -1,15 +1,13 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
 
 class CustomColors {
-  static const Color scaffoldBG = Colors.black38;
-  static const Color appBarBG = Colors.black26;
+  static const Color scaffoldBG = Colors.black87;
+  static const Color appBarBG = Colors.black;
   static const Color appBarText = Colors.white;
   static const Color cardBG = Colors.black45;
   static const Color cardText = Colors.white;
-  static const Color bgLight1 = Colors.black87;
-  static const Color bgLight2 = Colors.grey;
+  static const Color bgLight1 = Colors.black26;
+  static const Color bgLight2 = Colors.black12;
   static const Color dark = Colors.black;
   static const Color txtFieldbg = Colors.white38;
   static const Color hintDark = Color(0xff666874);
@@ -20,4 +18,19 @@ class CustomColors {
   static const Color redSecondary = Colors.redAccent;
   static const Color whitePrimary = Colors.white;
   static const Color whiteSecondary = Colors.white10;
+  static const Color borderColor = Color.fromARGB(255, 100, 0, 0);
+
+  static BoxDecoration cardDecoration = BoxDecoration(
+    gradient: const RadialGradient(
+      colors: [
+        Color.fromARGB(255, 55, 55, 55),
+        Color.fromARGB(255, 45, 45, 45),
+      ],
+    ),
+    border: Border.all(
+      color: CustomColors.borderColor,
+      width: 3,
+    ),
+    borderRadius: const BorderRadius.all(Radius.circular(20)),
+  );
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:portfolio/constants/colours.dart';
+import 'package:portfolio/constants/consts.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -11,8 +12,10 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  bool isMobile = false;
   @override
   Widget build(BuildContext context) {
+    isMobile = MediaQuery.of(context).size.width > 700 ? false : true;
     return Scaffold(
       backgroundColor: CustomColors.scaffoldBG,
       appBar: AppBar(
@@ -22,20 +25,66 @@ class _HomePageState extends State<HomePage> {
           "Portfolio",
           textAlign: TextAlign.center,
           style: GoogleFonts.playfair(
-            color: Colors.white,
+            color: CustomColors.appBarText,
             fontWeight: FontWeight.w900,
             fontSize: 28,
           ),
         ),
+        actions: isMobile
+            ? null
+            : [
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.home),
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.work),
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.contact_mail),
+                ),
+              ],
       ),
+      drawer: isMobile ? Drawer() : null,
       body: SafeArea(
+        minimum: const EdgeInsets.all(10),
         child: SingleChildScrollView(
           child: ListView(
             shrinkWrap: true,
             scrollDirection: Axis.vertical,
+            padding: const EdgeInsets.symmetric(vertical: 10),
             children: [
               //MAIN
-
+              Container(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 20,
+                  horizontal: 10,
+                ),
+                height: 400,
+                width: double.maxFinite,
+                decoration: CustomColors.cardDecoration,
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Text(
+                        "Hello, I'm\nKartik Kumar",
+                        style: GoogleFonts.dancingScript(
+                          color: CustomColors.whitePrimary,
+                          fontWeight: FontWeight.w900,
+                          fontSize: isMobile ? 26 : 32,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                    CircleAvatar(
+                      radius: isMobile ? 80 : 120,
+                      backgroundImage: const AssetImage("assets/profile.jpg"),
+                    ),
+                  ],
+                ),
+              )
               //SKILLS
 
               //PROJECTS
